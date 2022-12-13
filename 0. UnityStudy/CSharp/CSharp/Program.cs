6f517f544a43b95ab32cafbe2c6ae1784f9e89cb
Song minseok <ske3056@gmail.com>
2022-12-13 22:56:15 +0900

    unitystudy

---
+++ 0. UnityStudy/CSharp/CSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        //
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld!");
+            int hp;
+
+            //byte(1바이트) short(2바이트) int(4바이트) long(8바이트)
+            //sbytr ushort uint ulong
+
+
+            hp = 100;
+
+            int weight;
+            weight = 200;
+
+            Console.WriteLine("Hello World! {0}, {0}", hp, weight);
+        }
+    }
+}
