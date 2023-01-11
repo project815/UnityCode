3cc6acc15c05422f0208321c6699ae301ba86a9a
Song minseok <ske3056@gmail.com>
2023-01-11 23:57:32 +0900

    multi

---
+++ 0. UnityStudy/UnityC#study/ConsoleApp1/ConsoleApp7/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i <= 9; i++)
+            {
+                for(int j = 1; j <= 9; j++)
+                {
+                    Console.WriteLine(i + " * " + j + " = " + i * j);
+                }
+                Console.WriteLine("\n");
+            }    
+        }
+    }
+}
