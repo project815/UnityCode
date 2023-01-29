ad06377003690b2dabcc0c3b5256f6b0cc393c67
Song minseok <ske3056@gmail.com>
2023-01-29 18:43:45 +0900

    TextRPG1

---
+++ 0. UnityStudy/UnityC#study/ConsoleApp1/ConsoleApp20/Player.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    public enum PlayerType
+    {
+        None = 0,
+        Knight = 1,
+        Mage = 2,
+        Archer = 3,
+    }
+    internal class Player
+    {
+        protected PlayerType _type;
+        protected int hp = 0 ;
+        protected int attack = 0;
+
+        protected Player (PlayerType type)
+        {
+            _type = type;
+        }
+        public void SetInfo(int hp, int attack)
+        {
+            this.hp = hp;
+            this.attack = attack;
+        }
+        public int GetHp() { return hp; }
+        public int GetAttack() { return attack; }
+
+        public bool IsDead() 
+        {
+            return hp <= 0;
+        }
+        public void OnDamage(int attack)
+        {
+            hp -= attack;
+            if (hp < 0) hp = 0;
+        }
+    }
+    class Knight : Player
+    {
+        public Knight() : base(PlayerType.Knight)
+        {
+            SetInfo(100, 20);
+        }
+    }
+    class Mage : Player
+    {
+        public Mage() : base(PlayerType.Mage) 
+        {
+            SetInfo(100, 12);
+        }
+    }
+    class Archer : Player
+    {
+        public Archer() : base(PlayerType.Archer) 
+        {
+            SetInfo(200, 10);
+        }
+    }
+}
---
+++ 0. UnityStudy/UnityC#study/ConsoleApp1/ConsoleApp20/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp20;
+using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Knight();
+        }
+    }
+}
